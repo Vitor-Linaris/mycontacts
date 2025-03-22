ceb8c93fcc0d897bed5e980c14f3acca1438e701
Vitor-Linaris <vitor.linaris@gmail.com>
2025-03-22 18:18:42 -0300

    feat: add props in component Modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,11 +1,13 @@
+import PropTypes from 'prop-types';
+
 import { Container, Footer, Overlay } from './styles';
 
 import Button from '../Button';
 
-export default function Modal() {
+export default function Modal({ danger }) {
   return (
     <Overlay>
-      <Container>
+      <Container danger={danger}>
         <h1>Titulo do modal</h1>
         <p>
           Corpo do modal
@@ -13,9 +15,17 @@ export default function Modal() {
 
         <Footer>
           <button type="button" className="cancel-button">Cancelar</button>
-          <Button type="button">Deletar</Button>
+          <Button type="button" danger>Deletar</Button>
         </Footer>
       </Container>
     </Overlay>
   );
 }
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
