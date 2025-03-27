4722b28ab43cb9b09fd71b5473860376231afc9e
Vitor-Linaris <vitor.linaris@gmail.com>
2025-03-27 00:19:23 -0300

    feat: add improvements to email help

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -44,7 +44,7 @@ export default function ContactForm({ buttonLabel }) {
   }
 
   return (
-    <Form onSubmit={() => handleSubmit()}>
+    <Form onSubmit={() => handleSubmit()} noValidate>
       <FormGroup error={getErrorMessageByFieldName('name')}>
         <Input
           value={name}
@@ -55,6 +55,7 @@ export default function ContactForm({ buttonLabel }) {
       </FormGroup>
       <FormGroup error={getErrorMessageByFieldName('email')}>
         <Input
+          type="email"
           placeholder="E-mail"
           value={email}
           onChange={(e) => handleEmailChange(e)}
