499d0f9e000a61600a11cd457d4caeb7d909a822
Vitor-Linaris <vitor.linaris@gmail.com>
2025-06-17 17:14:44 -0300

    feat: add custom Error

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState, useMemo } from 'react';
+
 import {
   Container, Header, ListHeader, Card,
-  InputSearchContainer,
+  InputSearchContainer, ErrorContainer,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
+import sad from '../../assets/images/sad.svg';
 
 import Loader from '../../components/Loader';
+import Button from '../../components/Button';
+
 import ContactsService from '../../services/ContactsService';
 
 export default function Home() {
@@ -17,29 +21,29 @@ export default function Home() {
   const [orderBy, setOrderBy] = useState('asc');
   const [searchTerm, setSearchTerm] = useState('');
   const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   const filteredContacts = useMemo(() => contacts.filter(
     (contact) => contact.name.toLowerCase().includes(searchTerm.toLowerCase()),
   ), [contacts, searchTerm]);
 
-  useEffect(() => {
-    async function loadContacts() {
-      try {
-        setIsLoading(true);
+  async function loadContacts() {
+    try {
+      setIsLoading(true);
 
-        const contactsList = await ContactsService.listContacts(orderBy);
+      const contactsList = await ContactsService.listContacts(orderBy);
 
-        setContacts(contactsList);
-      } catch (error) {
-        console.error('Error: ', error);
-      } finally {
-        setIsLoading(false);
-      }
+      setHasError(false);
+      setContacts(contactsList);
+    } catch {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
     }
+  }
 
+  useEffect(() => {
     loadContacts();
-
-    return () => console.log('cleanup');
   }, [orderBy]);
 
   function handleToogleOrderBy() {
@@ -50,6 +54,10 @@ export default function Home() {
     setSearchTerm(event.target.value);
   }
 
+  const handleTryAgain = () => {
+    loadContacts();
+  };
+
   return (
     <Container>
       <Loader isLoading={isLoading} />
@@ -57,46 +65,71 @@ export default function Home() {
         <input value={searchTerm} type="text" placeholder="Pesquisar pelo nome..." onChange={handleChangeSearchTerm} />
       </InputSearchContainer>
 
-      <Header>
-        <strong>
-          {filteredContacts.length}
-          {filteredContacts.length === 1 ? ' contato' : ' contatos'}
-        </strong>
+      <Header hasError={hasError}>
+        {
+          !hasError && (
+            <strong>
+              {filteredContacts.length}
+              {filteredContacts.length === 1 ? ' contato' : ' contatos'}
+            </strong>
+          )
+        }
         <Link to="/new">Novo Contato</Link>
       </Header>
 
-      {filteredContacts.length > 0 && (
-        <ListHeader orderBy={orderBy}>
-          <button type="button" onClick={handleToogleOrderBy}>
-            <span>Nome</span>
-            <img src={arrow} alt="arrow" />
-          </button>
-        </ListHeader>
-      )}
+      {
+        hasError && (
+        <ErrorContainer>
+          <img src={sad} alt="sad" />
 
-      {filteredContacts.map((contact) => (
-        <Card key={contact.id}>
-          <div className="info">
-            <div className="contact-name">
-              <strong>
-                {contact.name}
-              </strong>
-              {contact.category_name && <small>{contact.category_name}</small>}
-            </div>
-            <span>{contact.email}</span>
-            <span>{contact.phone}</span>
-          </div>
+          <div className="details">
+            <strong>Ocorreu um erro ao obter os seus contatos!</strong>
 
-          <div className="actions">
-            <Link to={`/edit/${contact.id}`}>
-              <img src={edit} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
+            <Button type="button" onClick={handleTryAgain}>Tentar novamente</Button>
           </div>
-        </Card>
-      ))}
+        </ErrorContainer>
+        )
+      }
+
+      {
+        !hasError && (
+          <>
+            {filteredContacts.length > 0 && (
+            <ListHeader orderBy={orderBy}>
+              <button type="button" onClick={handleToogleOrderBy}>
+                <span>Nome</span>
+                <img src={arrow} alt="arrow" />
+              </button>
+            </ListHeader>
+            )}
+
+            {filteredContacts.map((contact) => (
+              <Card key={contact.id}>
+                <div className="info">
+                  <div className="contact-name">
+                    <strong>
+                      {contact.name}
+                    </strong>
+                    {contact.category_name && <small>{contact.category_name}</small>}
+                  </div>
+                  <span>{contact.email}</span>
+                  <span>{contact.phone}</span>
+                </div>
+
+                <div className="actions">
+                  <Link to={`/edit/${contact.id}`}>
+                    <img src={edit} alt="Edit" />
+                  </Link>
+                  <button type="button">
+                    <img src={trash} alt="Delete" />
+                  </button>
+                </div>
+              </Card>
+            ))}
+            ]
+          </>
+        )
+      }
 
     </Container>
   );
--- src/services/utils/httpClient.js
+++ src/services/utils/httpClient.js
@@ -1,4 +1,5 @@
 import delay from '../../utils/delay';
+import APIError from '../../errors/APIError';
 
 class HttpClient {
   constructor(baseUrl) {
@@ -6,11 +7,21 @@ class HttpClient {
   }
 
   async get(path) {
+    await delay(500);
     const response = await fetch(`${this.baseUrl}${path}`);
 
-    await delay(500);
+    const contentType = response.headers.get('Content-Type');
 
-    return response.json();
+    let body = null;
+    if (contentType.includes('application/json')) {
+      body = await response.json();
+    }
+
+    if (response.ok) {
+      return body;
+    }
+
+    throw new APIError(response, body);
   }
 }
 
